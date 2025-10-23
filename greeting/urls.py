88777fa29f13a4389439ec0149566339917e62b4
Raheshmonu123 <raheshraj344@gmail.com>
2025-10-23 17:34:04 +0530

    Implement greeting view and index template with dynamic object rendering; update URLs and style

--- greeting/urls.py
+++ greeting/urls.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+'''from django.contrib import admin
 from django.urls import path, include
 from . import views
 
@@ -7,5 +7,11 @@ urlpatterns = [
     path('forhome', views.forhome, name='forhome'),
     path('about', views.about, name='about'),
     
+]'''
+
+from django.urls import path
+from greeting import views
+urlpatterns = [
+  path('', views.greeting)
 ]
     
--- greeting/views.py
+++ greeting/views.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render
+'''from django.shortcuts import render
 
 def forhome(request):
     return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'about.html')'''
+    
+    
+from django.shortcuts import render
+
+
+def greeting(request):
+    my_objects = [
+        {'name': 'John', 'job':'WB' , 'salary': 10000,'time': 'full-time'},
+        {'name': 'Jane', 'job': 'AP', 'salary': 15000, 'time': 'part-time'},
+        {'name': 'Bob', 'job': 'SE', 'salary': 20000, 'time': 'full-time' },
+        {'name': 'Alice', 'job': 'DS', 'salary': 25000, 'time': 'full-time' }
+    ]
+    context = {'my_objects': my_objects}
+    return render(request, 'index.html', context)
 
